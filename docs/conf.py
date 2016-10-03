bfada571bb065552d2163f48ab0cb5a906d54752
Jon Wayne Parrott <jonwayne@google.com>
2016-10-02 19:22:27 -0700

    Change intersphinx mapping to py 3.5

--- docs/conf.py
+++ docs/conf.py
@@ -345,7 +345,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'https://docs.python.org/3.5': None}
 
 # Autodoc config
 autoclass_content = 'both'
